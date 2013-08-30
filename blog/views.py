01a0807ea2303cf4e5276a6e9600790eb0cc7cf8
nathanfloor <nathan.floor@gmail.com>
2013-08-30 17:01:02 +0200

    added small summmary of latest blog

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import RequestContext
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings
