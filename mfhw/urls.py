a076dbc6cb7d12be8cbfde5740162c43896eb382
Simon de Haan <simon@praekeltfoundation.org>
2013-09-03 16:17:01 +0200

    adding health check

--- mfhw/urls.py
+++ mfhw/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+
+def health(request):
+    return HttpResponse('ok')
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -14,6 +20,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^health/', health),
 
     # points to blog urls
     url(r'^', include('blog.urls', namespace="blog")),
