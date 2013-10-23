6fc0f2fd9ca5af206791276eedec14a22a69dfa0
gitumarkk <gitumarkk@gmail.com>
2013-10-23 15:06:59 +0200

    changed CharField to text field, made code really dry and added filtering for dat

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 # Create your models here.
-class Article(models.Model):    
+class Article(models.Model):
     def generate_new_filename(instance, filename):
         ext = os.path.splitext(filename)[1] # get file extension
         image_name = "%s%s" % (uuid.uuid1(), ext)
@@ -14,15 +14,27 @@ class Article(models.Model):
     title = models.CharField(max_length=200)
     default_img_path = "%s%s" % (settings.BLOG_IMGS_ROOT, 'default.jpg')
     image = models.ImageField(upload_to=generate_new_filename, default=default_img_path)
-    body = models.CharField(max_length=10000)
+    body = models.TextField()
     can_comment = models.BooleanField(default=False)
     pub_date = models.DateTimeField('date published')
 
     def __unicode__(self):
         return self.title
 
+    class Meta:
+        get_latest_by = "pub_date"
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'published recently?'
+
+
+    @classmethod
+    def get_latest_blogs(cls):
+        return Article.objects.filter(can_comment=True).order_by("-pub_date").all()
+
+    @classmethod
+    def get_latest_news(cls):
+        return Article.objects.filter(can_comment=False).order_by("-pub_date").all()
