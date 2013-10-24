99e6b699fa3d9bc3b7adaa8c4be404c65da18c04
Mike Jones <mike@westerncapelabs.com>
2013-10-24 16:33:45 +0200

    fixing upload permissions

--- mfhw/settings.py
+++ mfhw/settings.py
@@ -64,14 +64,14 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = (os.path.join(os.path.dirname(abspath()),
-             'blog',
-             'media'))
+MEDIA_ROOT = os.path.join(os.path.dirname(abspath(), 'blog', 'media'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = '/media/'
+
+FILE_UPLOAD_PERMISSIONS = 0644
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
